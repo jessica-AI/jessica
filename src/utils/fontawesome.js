c2ba21b2643f2e4087c75a5ce8134dac03f4f12a
Chuang <henrychuang1012@outlook.com>
2020-09-09 21:18:06 +0800

    fix huge icon

--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -1,5 +1,7 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
-library.add(faEnvelope,faLinkedin);
+config.autoAddCss = false;
+
+library.add(faEnvelope, faLinkedin);
